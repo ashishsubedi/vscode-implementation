9480af170b26df4b926c1500a514da5db3bc804b
Ashish <ashishsubedi10@gmail.com>
2018-02-12 10:18:35 +0545

    Updated

--- set_7_1.c
+++ set_7_1.c
@@ -1,18 +1,31 @@
 #include<stdio.h>
+double avg(double a[], int n);
+    int main()
+    {
 
-int main(){
+        int n, i;
+        double average, sum = 0;
+        scanf("%d", &n);
 
-    int n,i;
-    double avg,sum=0;
-    scanf("%d",&n);
+        double a[n];
+        for (i = 0; i < n; i++)
+        {
+            scanf("%lf", &a[i]);
+            // sum += a[i];
+        }
+        average = avg(a, n);
+        printf("The average is %lf\n", average);
 
-    double a[n];
-    for(i = 0; i< n; i++){
-        scanf("%lf",&a[i]);
-        sum += a[i];
+        return 0;
     }
-    avg = (sum)/n;
-    printf("The average is %lf\n",avg);
-
-    return 0;
-}
+    double avg(double a[], int n)
+    {
+        double s, c;
+        c = 0;
+        for (int i = 0; i <= n; i++)
+        {
+            c = c + a[i];
+        }
+        s = c / n;
+        return s;
+    }
